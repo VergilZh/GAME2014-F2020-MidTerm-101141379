622f0dbdd7a9dff0078adf781508455a9c97f6ef
VergilZh <z1205067147@gmail.com>
2020-10-23 13:19:01 -0400

    Without many modifications

--- Assets/_Scripts/IApplyDamage.cs
+++ Assets/_Scripts/IApplyDamage.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿/*
+    File Name: IApplyDamage.cs
+    Student Name: Han Zhan
+    Student ID: 101141379
+    Date last Modified: 2020/10/21
+    Program description: Check the damage done by the player to the enemy.
+    Revision History: No change.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
